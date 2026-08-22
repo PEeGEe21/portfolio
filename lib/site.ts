0535451b4ec978c082a5d13eff4f6bc0830cfb2e
praise <mailpraiseudeh@gmail.com>
2026-08-22 11:42:12 +0100

    chore: update site url

--- lib/site.ts
+++ lib/site.ts
@@ -1,8 +1,3 @@
-const fallbackSiteUrl = "http://localhost:3000";
-const vercelHost =
-  process.env.VERCEL_PROJECT_PRODUCTION_URL ?? process.env.VERCEL_URL;
-
 export const siteUrl = new URL(
-  process.env.NEXT_PUBLIC_SITE_URL ??
-    (vercelHost ? `https://${vercelHost}` : fallbackSiteUrl),
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://udeh-praise.vercel.app",
 );
